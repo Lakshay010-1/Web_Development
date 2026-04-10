cce7b2e35d47a310129953a2a8dd24aad7ff7647
Lakshay <black.leopard0987@gmail.com>
2026-04-10 05:46:53 +0530

    AI Chatbot Directory 0.2

--- React/AI_Chatbot_Directory/src/components/Footer.jsx
+++ React/AI_Chatbot_Directory/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <a href="#web-top"><img id="footer-icon" src="images/logo.png" /></a>
-            <h3 id="footer-credits">&copy;{new Date().getFullYear()} <a href="https://">LAKSHAY GOYAL</a></h3>
+            <h3 id="footer-credits">&copy;{new Date().getFullYear()} <a href="https://lakshaygoyal-lg-portfolio.vercel.app/">LAKSHAY GOYAL</a></h3>
         </footer>
     )
 }
