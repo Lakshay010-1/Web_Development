51dd061770f5d18f4c6d88a638640ade3016b1ba
Lakshay <black.leopard0987@gmail.com>
2026-07-02 09:16:51 +0530

    pin planet auth:re-configuration

--- React/pin-planet-v1/backend/controllers/userController.js
+++ React/pin-planet-v1/backend/controllers/userController.js
@@ -51,7 +51,7 @@ const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 3 * 24 * 60 * 60 * 1000
         });
 
