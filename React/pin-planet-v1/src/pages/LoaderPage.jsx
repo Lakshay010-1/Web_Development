1e40ead9156192da5615994f05c3aa8ca9947b70
Lakshay <black.leopard0987@gmail.com>
2026-07-02 07:07:24 +0530

    re-organized pin-planet files and folders

--- React/pin-planet-v1/src/pages/LoaderPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Loader from '../components/Loader'
-import styles from './styles/LoaderPage.module.css'
-
-export default function LoaderPage() {
-    return <div className={styles.container}>
-        <Loader />
-    </div>
-}
